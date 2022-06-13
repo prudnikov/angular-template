833b9337f8737e7e85213e7f695ae03d8ae4cd34
Vladimir Prudnikov <v.prudnikov@gmail.com>
2022-06-13 23:08:27 +0300

    Rename directory edit-modal -> item-edit-modal

--- src/app/listing/listing.module.ts
+++ src/app/listing/listing.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ListingComponent } from './listing.component';
 import { NgxSmartModalModule } from 'ngx-smart-modal';
-import { ItemEditModalComponent } from './edit-modal/item-edit-modal.component';
+import { ItemEditModalComponent } from './item-edit-modal/item-edit-modal.component';
 import { ItemsListComponent } from './items-list/items-list.component';
 import { FilterComponent } from './filter/filter.component';
 import { ConfirmDeleteModule } from '../confirm-delete/confirm-delete.module';
